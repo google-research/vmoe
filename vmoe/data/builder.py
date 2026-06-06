942a78bc81d2e63199f0933733582ddae0715416
Runze Wang <runze@google.com>
2026-06-06 03:44:09 -0700

    No public description
    PiperOrigin-RevId: 927717043

--- vmoe/data/builder.py
+++ vmoe/data/builder.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Google LLC.
+# Copyright 2026 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ import functools
 from typing import Optional, Sequence, Tuple
 
 import cachetools
+from google.protobuf import text_format
 import jax
 import tensorflow as tf
 import tensorflow_datasets.public_api as tfds
