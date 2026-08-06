c07681241f81ba11421ba98e523e1499b2738a79
Hana Joo <hanajoo@google.com>
2026-08-06 01:39:38 -0700

    No public description
    PiperOrigin-RevId: 960145876

--- vmoe/checkpoints/base.py
+++ vmoe/checkpoints/base.py
@@ -118,7 +118,7 @@ def iterate_complete_steps_for_prefix(
       sorted(steps_and_suffixes - incomplete_steps_and_suffixes,
              reverse=decreasing),
       lambda x: x[0]):
-    if not suffixes.difference(set(x[1] for x in group)):
+    if not suffixes.difference(set(x[1] for x in group)):  # pyrefly: ignore[missing-attribute]
       yield step
 
 
